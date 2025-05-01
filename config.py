7a1ea50575a66044be3b01fd47e53ab6c3ac2538
Arpan <drono0176@gmail.com>
2025-05-01 19:07:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "29899535"
+api_hash = "f90e1df9486cd7c26766e7387105e08e"
+bot_token = ""
+auth_users = [8143531643]
